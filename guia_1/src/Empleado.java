0f9fd10bbd78c286a365189025d765c346d37738
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-14 18:48:03 -0300

    clase desarrollada

--- guia_1/src/Empleado.java
+++ guia_1/src/Empleado.java
@@ -6,13 +6,85 @@ dependiendo del porcentaje que se le pase por parámetro. Considere crear un
 método que facilite imprimir por pantalla las características del objeto de la
 siguiente forma:
 Empleado[dni=?, nombre=?, apellido=?, salario=?]
-a. Inicialice un empleado Carlos Gutiérrez, con dni 23456345 y salario inicial de
-25000.
-b. Inicialice un empleado Ana Sánchez, con dni 34234123 y salario inicial de
-27500.
-c. Imprima ambos objetos por pantalla
-d. Aumente el salario del empleado Carlos en un 15% e imprima en pantalla el
-salario anual del mismo*/
+*/
+
+import java.util.UUID;
 
 public class Empleado {
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private double salario;
+
+    ////////////////////// CONSTRUCTORES
+    public Empleado() {
+        this.setDni();
+        this.nombre = "";
+        this.apellido = "apellido";
+        this.salario = 0;
+    }
+
+    public Empleado(String dni, String nombre, String apellido, double salario) {
+       // this.setDni();
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salario = salario;
+    }
+
+    ////////////////////// GETTERS
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+
+    ////////////////////// SETTERS
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    ////////////////////// OTROS
+
+    private void setDni(){
+
+        UUID aux_dni;
+        aux_dni = UUID.randomUUID();
+        this.dni = aux_dni.toString().substring(0, 9); //I just need a 10 digits long string
+
+    }
+    ////////////////////// OVERRIDDEN
+
+
+    @Override
+    public String toString() {
+        //Empleado[dni=?, nombre=?, apellido=?, salario=?]
+        return "Empleado[" +
+                "dni=" + dni +
+                ", nombre=" + nombre +
+                ", apellido=" + apellido +
+                ", salario=" + salario +
+                ']';
+    }
 }

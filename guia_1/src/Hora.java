fbba9e47663e6049f116b861c49a69537fed20c8
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-14 19:50:59 -0300

    clase planteada parcialmente

--- guia_1/src/Hora.java
+++ guia_1/src/Hora.java
@@ -3,13 +3,66 @@ Tenga en cuenta el rango de valores aceptados para cada uno de estos.
 a. Hora: 0 … 23
 b. Minuto: 0 … 59
 c. Segundo: 0 … 59
-Considere el siguiente comportamiento:
-1. Un método que imprima la hora bajo el siguiente formato hh:mm:ss
-usando zero a la izquierda ejemplo 13:04:22 .
-2. Un método que avance en 1 segundo y devuelva la instancia del objeto.
-3. Un método que retroceda en 1 segundo y devuelva la instancia del objeto.
-4. Instanciar el objeto y probar los métodos creados.*/
+*/
 
 
 public class Hora {
+    private int hora;
+    private int minutos;
+    private int segundos;
+
+    ////////////////////// CONSTRUCTORS
+
+    public Hora(int hora, int minutos, int segundos) {
+        if(checkHora(hora)) {
+            this.hora = hora;
+        }
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
+    ////////////////////// GETTERS
+
+    public int getHora() {
+        return hora;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    ////////////////////// SETTERS
+
+    public void setHora(int hora) {
+        if(checkHora(hora)) {
+            this.hora = hora;
+        }
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+
+    ////////////////////// OTROS
+    private boolean checkHora(int hora){
+        boolean flag =  false;
+        if (hora>=0 && hora<24){
+            flag = true;
+        }
+        return flag;
+    }
+
+    ////////////////////// OVERRIDDEN
+    @Override
+    public String toString() {
+        return "Son las " + hora + " : " + minutos + " : " + segundos;
+    }
 }

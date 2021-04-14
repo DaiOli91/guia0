53c3fa347dde26ff959d096a21b60f67d4f76e14
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-14 08:41:36 -0300

    creadas las clases Empleado e ItemVenta vacías- restan 3

---
+++ guia_1/src/Empleado.java
@@ -0,0 +1,18 @@
+/*
+* 2. Modele el objeto Empleado que posee las siguientes características, dni, nombre,
+apellido y salario. El mismo debe contar con la posibilidad de calcular el salario
+anual. A su vez se requiere otro método que permita aumentar el salario
+dependiendo del porcentaje que se le pase por parámetro. Considere crear un
+método que facilite imprimir por pantalla las características del objeto de la
+siguiente forma:
+Empleado[dni=?, nombre=?, apellido=?, salario=?]
+a. Inicialice un empleado Carlos Gutiérrez, con dni 23456345 y salario inicial de
+25000.
+b. Inicialice un empleado Ana Sánchez, con dni 34234123 y salario inicial de
+27500.
+c. Imprima ambos objetos por pantalla
+d. Aumente el salario del empleado Carlos en un 15% e imprima en pantalla el
+salario anual del mismo*/
+
+public class Empleado {
+}
---
+++ guia_1/src/ItemVenta.java
@@ -0,0 +1,11 @@
+/*
+* 3. Modele el objeto Ítem de Venta con las siguientes propiedades, identificador,
+descripción, cantidad y precio unitario. Cree los métodos para calcular el precio
+total teniendo en cuenta el precio unitario y cantidad. Un método que permita
+imprimir por pantalla los atributos del objeto de la siguiente forma:
+ItemVenta[id=?, descripción=?, cantidad=?, pUnitario=?, pTotal=?]
+a. Inicialice el objeto con los atributos necesarios
+b. Imprima por pantalla el objeto inicializado.*/
+
+public class ItemVenta {
+}

56e9b88dfa9900c2efd46d28f247ef31c7e3f539
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-14 18:47:48 -0300

    comentarios

--- guia_1/src/Rectangulo.java
+++ guia_1/src/Rectangulo.java
@@ -3,13 +3,7 @@
 1,0 por defecto. Este objeto debe exponer los getters y setters necesarios. También
 debe contar con un método para calcular el área y perímetro del mismo. Ejecute las
 siguientes pruebas:
-a. Inicializar un objeto Rectángulo estableciendo ancho y alto.
-b. Imprimir por pantalla el alto y ancho.
-c. Imprimir por pantalla el área y perímetro.
-d. Modificar el alto y el ancho de la instancia.
-e. Imprimir por pantalla nuevamente el cálculo del área y perímetro.
-f. Inicializar un objeto Rectángulo con los valores por defecto y verificar.
-* */
+ */
 
 public class Rectangulo {
 
